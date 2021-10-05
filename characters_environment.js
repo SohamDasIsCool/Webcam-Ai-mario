983ac779adfc9be2ad4446a2bf660090fd57bc1e
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-10-05 16:57:40 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -52,7 +52,14 @@ var gameConfig={
 /*====================================
 =            Game Status             =
 ====================================*/
+noseX="";
+noseY="";
+gameStatus="";
 
+function startGame(){
+  gameStatus="start";
+  document.getElementById("status").innerHTML="Status:Game is loading";
+}
 function game(){
 
   instializeInDraw();
--- main.js
+++ main.js
@@ -8,8 +8,23 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(620,250);
+	video.parent('game_console');
+	poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
-
+function modelLoaded(){
+	console.log("Model is loaded");
+}
+function gotPoses(results){
+	if(results.length>0){
+		console.log(results);
+		noseX=results[0].pose.nose.x;
+		noseY=results[0].pose.nose.y;
+		console.log("Nose X="+noseX+ " and Nose Y="+noseY);
+	}
+}
 function draw() {
 	game()
 }
